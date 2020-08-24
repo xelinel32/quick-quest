b1110bff65bd897cf0fdb7b1a8b65caab9ac7462
xelinel32 <artemsedlyar99@gmail.com>
2020-08-24 21:41:04 +0300

    create start template

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
 <template>
   <v-app>
-    <v-main></v-main>
+    <q-header />
+    <v-main>
+      <v-container>
+        <router-view name="default" />
+      </v-container>
+    </v-main>
+    <v-footer app dark padless>
+      <v-card class="flex" flat tile>
+        <v-card-title class="teal">
+          <v-row justify="center">
+            <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon>
+              <v-icon size="24px">{{ icon }}</v-icon>
+            </v-btn>
+          </v-row>
+        </v-card-title>
+        <v-card-text class="py-2 white--text text-center">
+          {{ new Date().getFullYear() }} — <strong>QuickQuestions 👍</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
+import qHeader from '@/components/qHeader'
 export default {
   name: 'App',
+  components: {
+    qHeader,
+  },
   data: () => ({
-    //
+    icons: ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram'],
   }),
 }
 </script>
