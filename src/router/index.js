8d6e36f0f1c42c048cfe448c2539e9b8ed342057
xelinel32 <artemsedlyar99@gmail.com>
2020-08-24 21:43:07 +0300

    setup route for page

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,36 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    name: 'questions',
+    path: '/',
+    component: () => import('../views/qQuestions.vue'),
+    meta: {
+      title: 'Questions',
+    },
+  },
+  {
+    name: 'contact',
+    path: '/contact',
+    component: () => import('../views/qContact.vue'),
+    meta: {
+      title: 'Contact',
+    },
+  },
+]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
+  linkActiveClass: 'teal--text',
+  linkExactActiveClass: '',
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  to.matched.forEach(item => (document.title = item.meta.title))
+  next()
 })
 
 export default router
