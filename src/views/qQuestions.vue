408e07d63d30d77d4dab76acd2c9ca4831da840e
xelinel32 <artemsedlyar99@gmail.com>
2020-08-24 21:41:21 +0300

    create question page

---
+++ src/views/qQuestions.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="q-questions">
+    <q-quest-form />
+  </div>
+</template>
+
+<script>
+import qQuestForm from '@/components/qQuestForm'
+export default {
+  name: 'qQuestions',
+  components: { qQuestForm },
+  data: () => ({}),
+}
+</script>
+
+<style lang="scss"></style>
