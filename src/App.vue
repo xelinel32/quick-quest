693b5dbef713700ee5710bc0065ad7c9a984d290
xelinel32 <artemsedlyar99@gmail.com>
2020-08-26 11:52:49 +0300

    generate different component for footer

--- src/App.vue
+++ src/App.vue
@@ -3,39 +3,25 @@
     <q-header />
     <v-main>
       <v-container>
-        <router-view name="default" />
+        <router-view />
       </v-container>
     </v-main>
-    <v-footer app dark padless>
-      <v-card class="flex" flat tile>
-        <v-card-title class="teal">
-          <v-row justify="center">
-            <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon>
-              <v-icon size="24px">{{ icon }}</v-icon>
-            </v-btn>
-          </v-row>
-        </v-card-title>
-        <v-card-text class="py-2 white--text text-center">
-          {{ new Date().getFullYear() }} — <strong>QuickQuestions 👍</strong>
-        </v-card-text>
-      </v-card>
-    </v-footer>
+    <q-footer />
   </v-app>
 </template>
 
 <script>
 import qHeader from '@/components/qHeader'
+import qFooter from '@/components/qFooter'
 export default {
   name: 'App',
   components: {
     qHeader,
+    qFooter,
   },
-  data: () => ({
-    icons: ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram'],
-  }),
 }
 </script>
 
 <style lang="scss">
-@import '~reset-css/sass/_reset.scss';
+/* @import '~reset-css/sass/_reset.scss'; */
 </style>
---
+++ src/components/qFooter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="q-footer">
+    <v-footer app dark absolute>
+      <v-row align="center">
+        <v-col>
+          <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon>
+            <v-icon size="24px">{{ icon }}</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col class="py-2 white--text text-right">
+          {{ new Date().getFullYear() }} — <strong>QQ 👍</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'qFooter',
+  data: () => ({
+    icons: ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram'],
+  }),
+}
+</script>
+
+<style lang="scss"></style>
